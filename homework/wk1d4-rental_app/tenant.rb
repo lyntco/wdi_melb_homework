4d2a983eaf9439a89f3173faa8c11f68f24366fa
lyntco <strawww@gmail.com>
2014-10-02 16:16:04 +1000

    Add rental app

---
+++ homework/wk1d4-rental_app/tenant.rb
@@ -0,0 +1,26 @@
+class Tenant
+  attr_reader :gender
+  attr_accessor :name, :age, :occupation, :is_funny
+
+  def initialize(tenant_details)
+    @name = tenant_details[:name]
+    @age = tenant_details[:age]
+    @gender = tenant_details[:gender]
+    @occupation = tenant_details[:occupation]
+    @is_funny = tenant_details[:is_funny]
+  end
+
+end
+
+# jenny_details = {
+#   :name => "Jenny",
+#   :age => 89,
+#   :gender => "Male",
+#   :occupation => "Works on the block",
+#   :is_funny => false
+# }
+
+# jenny = Tenant.new(jenny_details)
+
+# p jenny.name
+# p jenny.is_funny

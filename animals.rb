d928a4ec61441d53dd2e695c5e1bd9d56bd883f2
Jam Creencia <jrcreencia@gmail.com>
2014-10-05 10:42:32 +1100

    re inititalised git

---
+++ animals.rb
@@ -0,0 +1,21 @@
+class Animal
+	attr_accessor :name, :age, :gender, :species, :toys, :owner
+	def initialize(animal_details)
+		@name = animal_details[:name]
+		@age = animal_details[:age]
+		@gender = animal_details[:gender]
+		@species = animal_details[:species]
+		@owner = owner || "no one"
+		@toys = [] 
+
+	end
+
+	def to_s
+		print "#{@name} is a #{@age} year old #{@gender} #{@species} owned by #{@owner}. Toys: #{@toys.join(", ")}"
+	end
+
+	def to_s_option
+		print "#{@name}, the #{@age} year old #{@gender} #{@species}."
+	end
+
+end

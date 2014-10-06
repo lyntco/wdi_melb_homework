d33e51626171ea112a0f67edc5749fc895a856a0
Jam Creencia <jrcreencia@gmail.com>
2014-10-06 12:09:34 +1100

    grandma :working, clean

--- gumballs/jam_creencia/week2/wk2_day1/grandma.rb
+++ gumballs/jam_creencia/week2/wk2_day1/grandma.rb
@@ -3,22 +3,19 @@ class Grandma
 	end
 
 	def talk(input)
-		counter = 0
-		while counter != 2
+		counter = 1
+		while counter != 3
 				if input == input.upcase
 					year = Random.rand(1930..1950)
 					puts "NO, NOT SINCE #{year}"
 				elsif input == "bye"
 					puts "What's that honey, I didn't hear you.."
 					counter += 1
-				elsif input == "bye"
-					puts "OK bye!"
 				else 
 					puts "SPEAK UP SONNY JIM"
-
-				puts "Talk to Grandma:"
 			
 				end
+				puts "Talk to Grandma:"
 				input = gets.chomp
 				
 		end

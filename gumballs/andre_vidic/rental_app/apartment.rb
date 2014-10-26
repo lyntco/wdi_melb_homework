706543911867bfe0bd1b6ba0b2c476dd6b45e3ee
drej2k <andrevidic@hotmail.com>
2014-10-26 17:29:23 +1100

    partial rental app, will come back to this and complete

---
+++ gumballs/andre_vidic/rental_app/apartment.rb
@@ -0,0 +1,22 @@
+class Apartment
+  attr_accessor :tenants, :room_number, :number_bedrooms,  :number_bathrooms, :has_parking
+  def initialize(apartment_details)
+    @room_number = apartment_details[:room_number]
+    @number_bedrooms = apartment_details[:number_bedrooms]
+    @number_bathrooms = apartment_details[:number_bathrooms]
+    @has_parking = apartment_details[:has_parking]
+    @tenants = []
+  end
+
+end
+
+apartment_details = {
+  :room_number => 707,
+  :number_bedrooms => 1,
+  :number_bathrooms => 20,
+  :has_parking => true
+}
+
+a1 = Apartment.new(apartment_details)
+
+# p a1.number_bathrooms

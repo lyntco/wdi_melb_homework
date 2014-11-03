737e802beb764c65d00d2692b2d9da1cf24b0aec
Joshua Richardson <joshrichardson1234@hotmail.com>
2014-11-03 17:21:53 +1100

    Movie Search homework

---
+++ gumballs/joshua_richardson/wk3d3/movie_search3/main.rb
@@ -0,0 +1,48 @@
+require "sinatra"
+require "pry"
+require 'httparty'
+
+get '/' do
+  erb :form
+end
+
+# get '/search' do
+#     erb :form
+# end
+
+get '/search_list' do
+
+    params['site_search'].gsub!(" ", '+')
+
+    results = HTTParty.get("http://www.omdbapi.com/?s=#{params['site_search']}")
+
+    @movie_hash = JSON.parse(results)
+
+    @movies = @movie_hash["Search"]
+    binding.pry
+
+    erb :search_list
+end
+
+get '/index/:name' do
+
+    result = params[:name].gsub!(" ", '+')
+
+    results = HTTParty.get("http://www.omdbapi.com/?t=#{result}")
+
+    @movie_hash = JSON.parse(results)
+
+  erb :index
+
+end
+
+# get '/cloud' do
+
+#     params['site_search'].gsub!(/\s+/, '+')
+
+#     results = HTTParty.get("http://www.omdbapi.com/?t=#{params['site_search']}")
+
+#     @movie_hash = JSON.parse(results)
+
+#     erb :index
+# end
